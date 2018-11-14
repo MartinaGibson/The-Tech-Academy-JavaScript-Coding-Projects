d33aef9f2a01181ea394a47d05f48d2ae7ca903d
Martina Gibson <44233804+MartinaGibson@users.noreply.github.com>
2018-11-14 10:15:18 -0700

    Pizza Shop Demo Drill
    HTML, CSS, JavaScript to create a menu to order from and calculate total based on those selections

---
+++ final_drill_basic.js
@@ -0,0 +1,170 @@
+function getReceipt() {
+	// This initializes our string so it can get passed from  
+	// function to function, growing line by line into a full receipt
+	var text1 = "<h3>You Ordered:</h3>";
+	var runningTotal = 0;
+	var sizeTotal = 0;
+	var selectedSize = 0;
+	var sizeArray = document.getElementsByClassName("size");
+	
+	for (var i = 0; i < sizeArray.length; i++) {
+		if (sizeArray[i].checked) {
+			selectedSize = sizeArray[i].value;
+			text1 = text1+selectedSize+"<br>";
+		}
+	}
+	
+	if (selectedSize === "Personal Pizza") {
+		sizeTotal = 6;
+	} else if (selectedSize === "Medium Pizza") {
+		sizeTotal = 10;
+	} else if (selectedSize === "Large Pizza") {
+		sizeTotal = 14;
+	} else if (selectedSize === "Extra Large Pizza") {
+		sizeTotal = 16;
+	}
+	
+	runningTotal = sizeTotal;
+	getMeat(runningTotal,text1); // All three of these variables will be passed on to each function
+
+	
+};
+
+function getMeat(runningTotal,_text1) {
+	var meatTotal= 0;
+	var selectedMeat = [];
+	var meatArray = document.getElementsByClassName("meats");
+
+	for (var j = 0; j < meatArray.length; j++) {
+		if (meatArray[j].checked) {
+	
+			selectedMeat.push(meatArray[j].value);
+			_text1 = _text1+meatArray[j].value+"<br>";
+		}
+	}
+		
+	var meatCount = selectedMeat.length;
+	if (meatCount > 1) {
+		meatTotal = (meatCount - 1);
+	} else {
+		meatTotal = 0;
+	}
+			
+	runningTotal = (runningTotal + meatTotal);
+	
+	getVeggie(runningTotal, _text1);
+
+	
+};	
+
+function getVeggie(runningTotal,_text1) {
+	var veggieTotal =0;
+	var selectedVeggie = [];
+	var veggieArray = document.getElementsByClassName("veggie");
+	
+	for (var v = 0; v < veggieArray.length; v++) {
+		if (veggieArray[v].checked) {
+			selectedVeggie.push(veggieArray[v].value);
+
+			_text1 = _text1+veggieArray[v].value+"<br>";
+		}
+	}
+	var veggieCount = selectedVeggie.length;
+	if (veggieCount > 1) {
+		veggieTotal = (veggieCount - 1);
+	} else {
+		veggieTotal = 0;
+	}
+		
+	runningTotal = (runningTotal + veggieTotal);	
+	
+	getCrust(runningTotal,_text1);
+
+};
+
+function getCrust(runningTotal,_text1) {
+	var crustTotal = 0;
+	var selectedCrust = 0;
+	var crustArray = document.getElementsByClassName("crust");
+		
+	for (var c = 0; c < crustArray.length; c++) {
+		if (crustArray[c].checked) {
+			selectedCrust = crustArray[c].value;
+			_text1 = _text1+selectedCrust+"<br>";	
+		} 
+	}
+		
+	if (selectedCrust === "Plain Crust") {
+		crustTotal = 0;
+	} else if (selectedCrust === "Garlic Buttery Crust") {
+		crustTotal = 0;
+	} else if (selectedCrust === "Cheesy Stuffed Crust") {
+		crustTotal = 3;
+	} else if (selectedCrust === "Spicy Crust") {
+		crustTotal = 0;
+	} else if (selectedCrust === "House Tossed Crust"){
+		crustTotal= 0;
+	}
+		
+	runningTotal = (runningTotal + crustTotal);
+	
+	getSauce(runningTotal,_text1);
+
+};
+
+function getSauce(runningTotal,_text1) {
+	var sauceTotal = 0;
+	var selectedSauce = 0;
+	var sauceArray = document.getElementsByClassName("sauce");
+	
+	for (var s = 0; s < sauceArray.length; s++) {
+		if (sauceArray[s].checked) {
+			selectedSauce = sauceArray[s].value;
+			_text1 = _text1+selectedSauce+"<br>";	
+		} 
+	}
+
+	if (selectedSauce === "Red Marinara Sauce") {
+		sauceTotal = 0;
+	} else if (selectedSauce === "White Garlic Sauce") {
+		sauceTotal = 0;
+	} else if (selectedSauce === "BBQ Sauce") {
+		sauceTotal = 0;
+	} else if (selectedSauce === "No Sauce") {
+		sauceTotal = 0;
+	}
+		
+	runningTotal = (runningTotal + sauceTotal);
+	getCheese(runningTotal,_text1);
+	
+};
+
+function getCheese(runningTotal,_text1){
+	var cheeseTotal;
+	var selectedCheese;
+	var cheeseArray = document.getElementsByClassName("cheese");
+		
+	for (var e = 0; e < cheeseArray.length; e++) {
+		if (cheeseArray[e].checked) {
+			selectedCheese = cheeseArray[e].value;
+			_text1 = _text1+selectedCheese+"<br>";
+		}
+	}
+			
+	if (selectedCheese ==="Regular Cheese") {
+		cheeseTotal = 0;
+	} else if (selectedCheese === "Xtra Cheese") {
+		cheeseTotal = 3; 
+	} else if (selectedCheese === "No Cheese") {		
+		cheeseTotal = 0;
+	}	
+	
+	runningTotal = (runningTotal + cheeseTotal);
+	
+	document.getElementById("totalPrice").innerHTML = "</h3>Total: <strong>$"+runningTotal+".00"+"</strong></h3>";	
+	document.getElementById("showText").innerHTML = _text1;
+	
+};
+
+
+
